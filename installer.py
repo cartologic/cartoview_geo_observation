5f87d01103934ab5021fe5c34cfcadf7fb4c2ee0
hisham waleed karam <hisham.karam@cartologic.com>
2017-09-21 13:40:41 +0200

    add new description

--- installer.py
+++ installer.py
@@ -3,7 +3,11 @@ __author__ = "cartologic"
 
 info = {
     "title": "GeoObservation",
-    "description": "collect XY Location and associate Attributes and Images. Work Field Crews Can utilize this Application To Collect Information About Assets .Users can also use this Application To report Issues or Collect New Features . once the data is Saved it Will Be directly Saved To the server, to setup your app author a new shapefile then upload it to the server then follow the instructions, users will use the app to save data to this layer",
+    "description": """Users can submit new reports, review existing reports, and comment and vote on reports or observations submitted by other users. They can track the status of problems or observations they have reported.
+
+Field observations and mobile data collection, whether by professional monitoring staff or by volunteers in e.g. citizen science and mobile crowdsourcing projects. Field observations app is also useful as a platform for building a variety of mobile-first websites and CRUD applications.Allows users to submit problems or observations. 
+
+The application has been optimized for smartphones but is responsively designed to be used on smartphones, tablets, and desktop computers.""",
     "author": "Cartologic",
     "home_page": "http://cartoview.org/apps/test",
     "help_url": "http://cartoview.org/apps/test/help/",
