f213436832ff5e918b65251d4838d61c12f92f72
khaled <bokae97@gmail.com>
2020-12-21 13:31:05 +0200

    fixed: missing on_delete attribute

--- models.py
+++ models.py
@@ -6,10 +6,12 @@ from geonode.layers.models import Layer
 
 class UserHistory(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name="collector_history")
+                             related_name="collector_history",
+                             on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     update = models.DateTimeField(auto_now=True, auto_now_add=False)
-    layer = models.ForeignKey(Layer, related_name="collector_history")
+    layer = models.ForeignKey(Layer, related_name="collector_history",
+                              on_delete=models.CASCADE)
     fid = models.TextField(blank=True, null=True)
     data = models.TextField(blank=False, null=False)
 
